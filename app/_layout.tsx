aa3913c990854069fbf4dbe36218a62e5b684422
Timothy Lee <klee616@se14l0124-18.ad.bcit.ca>
2024-10-04 09:51:42 -0700

    first commmit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import "../global.css"
+
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
